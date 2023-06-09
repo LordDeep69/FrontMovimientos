bc09a41e9c074d038ca4e76004ced5024b17d626
LordDeep <lordeep3@gmail.com>
2023-06-09 10:43:41 -0500

    COMMIT MODULES

--- script.js
+++ script.js
@@ -1,35 +1,4 @@
-const URL_API = "https://movimientos.onrender.com/movimientos"
-const main = document.getElementById("container");
-const header = document.getElementById("cabecera");
-const body = document.getElementById("body");
-const getMovimientos = async () => {
-    try 
-    {
-     
-        const {data} = await axios.get(URL_API);
-        const th = Object.keys(data[0]);
-        for (let i = 0; i < th.length; i++)
-        {
-            
-            header.innerHTML += `
-            <th class = "th">${th[i]} </th>`
-
-            body.innerHTML += `
-            <tr class = "tr">
-                <td class = "td">${data[i].id}</td>
-                <td class = "td">${data[i].description}</td>
-                <td class = "td">${data[i].precio}</td>
-                <td class = "td">${data[i].type}</td>
-                <td class = "td">${data[i].action}</td>
-
-            </tr>`
-        }
-
-    } catch (error) {
-        console.log(error);
-        return [];
-    }
-}
+import  {getMovimientos}  from "./services/getMovimientos.js";
 
 document.addEventListener("DOMContentLoaded", async()=>{
     const movimientos  = await getMovimientos();
---
+++ services/getMovimientos.js
@@ -0,0 +1,33 @@
+const URL_API = "https://movimientos.onrender.com/movimientos"
+const header = document.getElementById("cabecera");
+const body = document.getElementById("body");
+
+export const getMovimientos = async () => {
+    try 
+    {
+     
+        const {data} = await axios.get(URL_API);
+        const th = Object.keys(data[0]);
+        for (let i = 0; i < th.length; i++)
+        {
+            
+            header.innerHTML += `
+            <th class = "th">${th[i]} </th>`
+
+            body.innerHTML += `
+            <tr class = "tr">
+                <td class = "td">${data[i].id}</td>
+                <td class = "td">${data[i].description}</td>
+                <td class = "td">${data[i].precio}</td>
+                <td class = "td">${data[i].type}</td>
+                <td class = "td">${data[i].action}</td>
+
+            </tr>`
+        }
+
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
+
